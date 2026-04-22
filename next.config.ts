39092865d4a341d419cb27ba08840ef87744b249
Kumaraguru-14 <guru.kumara@innovasolutions.com>
2026-04-22 11:02:23 +0530

    Config Added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  poweredByHeader: false,
+  turbopack: {
+    resolveAlias: {
+      '@': './src',
+      '@public': './public',
+    },
+  },
+  images: {
+    qualities: [25, 50, 75, 100],
+    unoptimized: true,
+  },
+  compiler: {
+    removeConsole: {
+      exclude: ['error', 'warn'],
+    },
+  },
+  experimental: {
+    optimizeCss: true,
+  },
+
+  output: "export",
 };
 
 export default nextConfig;
